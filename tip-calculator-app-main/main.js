525341993bc145a2d84fd065c0ecc14e6f96e0d3
shempill24 <105935935+shempill24@users.noreply.github.com>
2022-07-10 16:18:07 -0400

    Add files via upload

---
+++ tip-calculator-app-main/main.js
@@ -0,0 +1,5 @@
+const billInput = document.getElementById("#bill_input").value;
+const pplInput = document.getElementById("#ppl_input").value;
+const buttons = document.querySelectorAll(".perc");
+
+alert(billInput);
